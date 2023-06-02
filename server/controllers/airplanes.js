f091012baa046ec78c4b293b0cd1bc4df1973b84
refvds <refvdsk@gmail.com>
2023-06-03 00:09:54 +0300

    feat: add field validation

--- server/controllers/airplanes.js
+++ server/controllers/airplanes.js
@@ -10,6 +10,42 @@ const getAirplanes = async(req, res) => {
 };
 
 const createAirplane = async(req, res) => {
+    const errors = {};
+
+    if (!req.body.name) {
+        errors.name = {
+            message: 'This field must be filled in'
+        }
+    }
+
+    if (!req.body.price) {
+        errors.price = {
+            message: 'This field must be filled in'
+        }
+    }
+
+    if (!req.body.description) {
+        errors.description = {
+            message: 'This field must be filled in'
+        }
+    }
+
+    if (req.body.description && req.body.description.length > 700) {
+        errors.description = {
+            message: 'The length of this field must be 700 or less '
+        }
+    }
+
+    if (!req.body.copacity) {
+        errors.copacity = {
+            message: 'This field must be filled in'
+        }
+    }
+
+    if (Object.keys(errors.length > 0)) {
+        return res.status(400).json(errors);
+    }
+
     try {
         const { name, price, description, copacity } = req.body;
         console.log(req.body)
