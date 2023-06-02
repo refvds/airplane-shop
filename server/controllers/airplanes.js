ab83c57b22055c422edbc5f87ab73823501d3f38
refvds <refvdsk@gmail.com>
2023-06-02 23:46:24 +0300

    feat: add create airplane functionality by post method

--- server/controllers/airplanes.js
+++ server/controllers/airplanes.js
@@ -5,10 +5,29 @@ const getAirplanes = async(req, res) => {
         const airplanes = await Airplane.find();
         res.status(200).json(airplanes);
     } catch (error) {
-        res.status(500).json({error: 'Internal Server Error'})
+        res.status(500).json({error: 'Internal Server Error'});
     }
 };
 
+const createAirplane = async(req, res) => {
+    try {
+        const { name, price, description, copacity } = req.body;
+        console.log(req.body)
+        const airplane = await Airplane.create({
+            name, 
+            price, 
+            description, 
+            copacity,
+            image:  `http://localhost:${process.env.PORT}/static/${req.file.filename}` 
+        });
+        res.status(201).json(airplane);
+    } catch(error) {
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+}
+
+
 module.exports = {
-    getAirplanes
+    getAirplanes,
+    createAirplane
 }
--- server/routes/airplanes.js
+++ server/routes/airplanes.js
@@ -1,14 +1,21 @@
 const path = require('path')
 const express = require('express');
-
-const {getAirplanes} = require('../controllers/airplanes');
-
-
+const multer = require('multer');
+const { getAirplanes, createAirplane } = require('../controllers/airplanes');
 
 const router = express.Router();
 
+const storage = multer.diskStorage({
+    destination: './assets/',
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+});
+
+const upload = multer({ storage });
+
 router.get('/', getAirplanes);
 router.get('/:id', (req, res)=>res.send('Get an airplane'));
-router.post('/', (req, res)=>res.send('post an airplane'));
+router.post('/', upload.single('image'), createAirplane);
 
 module.exports = router;
