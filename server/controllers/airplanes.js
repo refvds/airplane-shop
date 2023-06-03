2332086216e2cb0a75efad931a341b538d743621
refvds <refvdsk@gmail.com>
2023-06-03 16:03:04 +0300

    fix: a bug with Object keys length that was incorrect and genereted falsy 400 status code

--- server/controllers/airplanes.js
+++ server/controllers/airplanes.js
@@ -20,7 +20,7 @@ const getAirplane = async(req, res) => {
 
 const createAirplane = async(req, res) => {
     const errors = {};
-
+    
     if (!req.body.name) {
         errors.name = {
             message: 'This field must be filled in'
@@ -45,26 +45,26 @@ const createAirplane = async(req, res) => {
         }
     }
 
-    if (!req.body.copacity) {
-        errors.copacity = {
+    if (!req.body.capacity) {
+        errors.capacity = {
             message: 'This field must be filled in'
         }
     }
 
-    if (Object.keys(errors.length > 0)) {
+    if (Object.keys(errors).length > 0) {
         return res.status(400).json(errors);
     }
 
     try {
-        const { name, price, description, copacity } = req.body;
-        console.log(req.body)
+        const { name, price, description, capacity } = req.body;
         const airplane = await Airplane.create({
             name, 
             price, 
             description, 
-            copacity,
+            capacity,
             image:  `http://localhost:${process.env.PORT}/static/${req.file.filename}` 
         });
+        
         res.status(201).json(airplane);
     } catch(error) {
         res.status(500).json({error: 'Internal Server Error'});
